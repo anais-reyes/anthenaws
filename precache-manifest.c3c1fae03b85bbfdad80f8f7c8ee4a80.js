5eca8b03b3a472583b4f4341921896d38b9605f7
Anais <maria.reye@itexico.com>
2019-08-12 21:24:39 -0500

    Updates

---
+++ precache-manifest.c3c1fae03b85bbfdad80f8f7c8ee4a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155e6f3b7d809759c7604f7e7b6b1021",
+    "url": "/anthenaws/index.html"
+  },
+  {
+    "revision": "a1add909d45c5a960f75",
+    "url": "/anthenaws/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "892715b5f7613dc50f99",
+    "url": "/anthenaws/static/css/main.1a85ea09.chunk.css"
+  },
+  {
+    "revision": "a1add909d45c5a960f75",
+    "url": "/anthenaws/static/js/2.551300b8.chunk.js"
+  },
+  {
+    "revision": "892715b5f7613dc50f99",
+    "url": "/anthenaws/static/js/main.d3240222.chunk.js"
+  },
+  {
+    "revision": "386d0176d2ff7c6642fd",
+    "url": "/anthenaws/static/js/runtime~main.691c7b9b.js"
+  }
+]);
